a6bc159e7fae1db55b21730d777e637870912489
darshanakgr <darshanakgr@gmail.com>
2022-02-05 13:20:12 +0800

    customize layout

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, url_for, render_template
-
+from configs import colors
 
 app = Flask(__name__)
 
@@ -7,7 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-
+@app.route('/customize')
+def customize():
+    return render_template("customize.html", primary_colors=colors.primary, secondary_colors=colors.secondary)
 
 
 if __name__ == "__main__":
